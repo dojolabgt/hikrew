95ff3809c003190eafe23a7a06d9e9397d0548ee
Pablo Lacán <pablo.lacan@galileo.edu>
2026-03-18 06:54:08 -0600

    feat: Add `googleId` column with a unique constraint to the `users` table.

--- backend/src/migrations/1773910000000-AddGoogleIdToUsers.ts
+++ backend/src/migrations/1773910000000-AddGoogleIdToUsers.ts
@@ -8,7 +8,7 @@ export class AddGoogleIdToUsers1773910000000 implements MigrationInterface {
       `ALTER TABLE "users" ADD COLUMN IF NOT EXISTS "googleId" character varying(255) DEFAULT NULL`,
     );
     await queryRunner.query(
-      `ALTER TABLE "users" ADD CONSTRAINT IF NOT EXISTS "UQ_f382af58ab36057334fb262efd5" UNIQUE ("googleId")`,
+      `ALTER TABLE "users" ADD CONSTRAINT "UQ_f382af58ab36057334fb262efd5" UNIQUE ("googleId")`,
     );
   }
 
